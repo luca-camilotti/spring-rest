c1a2dc26c85cbf4293efd29143111e1ae50f1004
luca.camilotti <luca.camilotti@gmail.com>
2023-05-27 15:36:55 +0200

    post new employee

--- src/main/java/com/supersballo/restservice/ApiController.java
+++ src/main/java/com/supersballo/restservice/ApiController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,5 +49,16 @@ public class ApiController {
    public Optional<Employee> getEmployeeById(@PathVariable("id") int id) {
       return service.getById(id);
    }
+   
+   /* Managing post requests (example) 
+    * This method adds a new employee 
+    * to the database.
+    * 
+    * */
+   @PostMapping("/employees")
+   public Employee registerNewEmployee(@RequestBody Employee emp) {
+      return service.addNewEmployee(emp);
+   }
+
 
 }
--- src/main/java/com/supersballo/restservice/MyService.java
+++ src/main/java/com/supersballo/restservice/MyService.java
@@ -8,18 +8,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
-   
-   private final MyRepository repo;
-   @Autowired 
-   public MyService(MyRepository rp) {
-      this.repo = rp;
-   }
 
-   public List<Employee> getEmployees() {
-      return repo.findAll();
-   }
-   public Optional<Employee> getById(int id) {
-	   return repo.findById((long) id);
-   }
+	private final MyRepository repo;
+	@Autowired 
+	public MyService(MyRepository rp) {
+		this.repo = rp;
+	}
+
+	public List<Employee> getEmployees() {
+		return repo.findAll();
+	}
+	public Optional<Employee> getById(int id) {
+		return repo.findById((long) id);
+	}
+
+	public Employee addNewEmployee(Employee emp) {
+		return repo.save(emp);  // use the repository built-in save() method
+	}
 }
 
